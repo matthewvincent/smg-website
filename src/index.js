ba6051deafa7eaecea30198421837b5658a6aabb
Matthew Vincent <matthewlawrencevincent@gmail.com>
2018-04-07 14:50:19 -0700

    refactor to hashHistory

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
 import Home from './Home';
 import Contact from './Contact';
 import Producers from './Producers';
 import Touring from './Touring';
 import './App.css'
 
+const History = createHashHistory();
+
 ReactDOM.render(
   <div className="App">
-    <Router>
+    <Router history={History}>
       <div>
         <App />
         <Switch>
