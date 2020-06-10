17d7ea0c3798028eec2691c246a057b3e9046c7e
Matthew Vincent <matthew.vincent@veeva.com>
2020-06-10 15:44:46 -0700

    padding around gigwell stuff

--- src/GigwellWrapper.js
+++ src/GigwellWrapper.js
@@ -26,23 +26,25 @@ class GigwellWrapper extends React.Component {
   }
 
   render() {  
-    return [
-      <Spinner visible={this.state.showSpinner} />,
-      <gigwell-embedded-roster
-        id="gigwell-iframe-container"
-        agency="162101"
-        button-text-color="#fff"
-        hide-past-events="true"
-        hide-upcoming-events="true"
-        highlight-color="#e2502c"
-        labels="{'backToRoster':'Back to Roster','bioHideReadMore':'Hide Full Bio','bioReadMore':'Read Full Bio','bookNow':'Book Now','downloads':'Downloads','genres':'Genres','labels':'Label','loading':'Loading...','loadingEvents':'Loading Events...','location':'Location','noPastEvents':'No past Events.','noUpcomingEvents':'No upcoming gigs.','pastEvents':'Past Events','pressKit':'Press Kit','rsvp':'RSVP','techRider':'Tech Rider','tickets':'Tickets','upcomingEvents':'Upcoming gigs'}"
-        profile-box-shadow-color="#ccc"
-        profile-text-color="#333"
-        roster-text-color="#333"
-        roster-views="[{'borderRadius':3,'enabled':true,'imageHeight':100,'size':300,'style':'none','type':'list'}]"
-        tags={this.props.tags}
-      ></gigwell-embedded-roster>
-    ];
+    return (
+      <div className="gigwell-wrapper">
+        <Spinner visible={this.state.showSpinner} />
+        <gigwell-embedded-roster
+          id="gigwell-iframe-container"
+          agency="162101"
+          button-text-color="#fff"
+          hide-past-events="true"
+          hide-upcoming-events="true"
+          highlight-color="#e2502c"
+          labels="{'backToRoster':'Back to Roster','bioHideReadMore':'Hide Full Bio','bioReadMore':'Read Full Bio','bookNow':'Book Now','downloads':'Downloads','genres':'Genres','labels':'Label','loading':'Loading...','loadingEvents':'Loading Events...','location':'Location','noPastEvents':'No past Events.','noUpcomingEvents':'No upcoming gigs.','pastEvents':'Past Events','pressKit':'Press Kit','rsvp':'RSVP','techRider':'Tech Rider','tickets':'Tickets','upcomingEvents':'Upcoming gigs'}"
+          profile-box-shadow-color="#ccc"
+          profile-text-color="#333"
+          roster-text-color="#333"
+          roster-views="[{'borderRadius':3,'enabled':true,'imageHeight':100,'size':300,'style':'none','type':'list'}]"
+          tags={this.props.tags}
+        ></gigwell-embedded-roster>  
+      </div>
+    )
   }
 }
 
